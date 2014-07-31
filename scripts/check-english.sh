0dacf85500ccbe4ae3f7f003c1f5866f4793441d
Jennifer Warrender <jaydchan@gmail.com>
2014-07-31 03:49:17 +0100

    simplified as check for atleast one letter was not required

--- scripts/check-english.sh
+++ scripts/check-english.sh
@@ -6,15 +6,6 @@ outfile=./output/cenglish.txt
 
 cat $infile | tr '[:upper:]' '[:lower:]' > $outfile
 
-# should exempt dash (-)
 cat $outfile | tr -s [:space:][:punct:] \\n | sort -u> $temp
-
-while read word
-do
-    if echo $word | grep -q "[a-z]"
-    then
-	echo $word >> $outfile
-    fi
-done <$temp
 
 rm $temp
