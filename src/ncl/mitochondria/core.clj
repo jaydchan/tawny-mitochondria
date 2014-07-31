9cfdbda7723b1f5d2ce4146a84338c93aca5c954
Jennifer Warrender <jaydchan@gmail.com>
2014-07-31 03:39:22 +0100

    started cq incorporation

--- src/ncl/mitochondria/core.clj
+++ src/ncl/mitochondria/core.clj
@@ -21,12 +21,13 @@
         [clojure.java.shell :only [sh]])
   (:require [ncl.mitochondria
              refine
-             ;; paper body mitochondria component
+             paper body mitochondria component
              disease gene protein
              ;; mutation
-             ;; term
+             term
              omim
-             ;; graph
+             cq
+             graph
              ])
   (:gen-class))
 
@@ -61,15 +62,15 @@
   ;; (save-ontology ncl.mitochondria.mutation/mutation "mutation.omn" :omn)
   ;; (save-ontology ncl.mitochondria.mutation/mutation "mutation.owl" :owl)
 
-  ;; ;; refine lists
+  ;; refine lists
   ;; (println "Refining lists: Start")
   ;; (sh "./scripts/make-wordlist.sh")
   ;; (sh "./scripts/check-english.sh")
   ;; (println "Refining lists: Loading...")
   ;; (ncl.mitochondria.refine/driver)
-  ;; (println "Refining lists: Complete.")
+  ;; (println "Refining lists: Complete")
 
-  ;; ;; generate term classes
+  ;; generate term classes
   ;; (println "Generating term classes: Start")
   ;; (ncl.mitochondria.term/driver)
   ;; (save-ontology ncl.mitochondria.term/term "term.omn" :omn)
@@ -77,15 +78,21 @@
   ;; (println "Generating term clases: Complete.")
 
   ;; incorporate omim relations
-  (println "Incorporating OMIM relations: Start")
-  (ncl.mitochondria.omim/driver)
-  (save-ontology ncl.mitochondria.disease/disease "disease.omn" :omn)
-  (save-ontology ncl.mitochondria.disease/disease "disease.owl" :owl)
+  ;; (println "Incorporating OMIM relations: Start")
+  ;; (ncl.mitochondria.omim/driver)
+  ;; (save-ontology ncl.mitochondria.disease/disease "disease.omn" :omn)
+  ;; (save-ontology ncl.mitochondria.disease/disease "disease.owl" :owl)
+  ;; (println "Incorporating OMIM relations: Complete")
 
-  (println "Incorporating OMIM relations: Complete.")
+  ;; cqs
+  ;; (println "Incorporating cq queries: Start")
+  ;; (ncl.mitochondria.cq/driver)
+  ;; (save-ontology ncl.mitochondria.cq/cq "cq.omn" :omn)
+  ;; (save-ontology ncl.mitochondria.cq/cq "cq.owl" :owl)
+  ;; (println "Incorporating cq queries: Complete")
 
-  ;; ;; generate graphs
+  ;; generate graphs
   ;; (println "Generating graphs: Start")
   ;; (ncl.mitochondria.graph/driver)
-  ;; (println "Generating graphs: Complete.")
+  ;; (println "Generating graphs: Complete")
 )
---
+++ src/ncl/mitochondria/cq.clj
@@ -0,0 +1,82 @@
+;; The contents of this file are subject to the LGPL License, Version 3.0.
+
+;; Copyright (C) 2014, Newcastle University
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see http://www.gnu.org/licenses/.
+
+(ns ^{:doc "TODO"
+      :author "Jennifer Warrender"}
+  ncl.mitochondria.cq
+  (:use [tawny.owl])
+  (:require [ncl.mitochondria
+             [generic :as g]
+             [paper :as ppr]
+             [body :as b]
+             [mitochondria :as mit]
+             [component :as c]
+             [disease :as d]
+             [gene :as gne]
+             [protein :as pro]
+             ;; [mutation :as mut]
+             [term :as t]
+             ]))
+
+;; new ontology
+(defontology cq
+  :iri "http://ncl.ac.uk/mitochondria/cq"
+  :prefix "cq:")
+
+;; import other ontologies
+(owl-import ppr/paper)
+(owl-import b/body)
+(owl-import mit/mitochondria)
+(owl-import c/component)
+(owl-import d/disease)
+(owl-import gne/gene)
+(owl-import pro/protein)
+;; (owl-import mut/mutation)
+
+;; OWL CLASSES
+(defclass Competency_Question)
+
+;; PATTERNS
+(defn create-cq
+  [name cquestion]
+  (owl-class cq
+             name
+             :label cquestion
+             :subclass Competency_Question))
+
+;; MAIN
+(defn driver
+  []
+
+  ;; read files
+  (let [refined (into [] (g/get-lines
+                          "./output/cqs/refined_cq.txt"))
+        quarantined (into [] (g/get-lines
+                              "./output/cqs/quarantined_cq.txt"))
+        cqs (merge refined quarantined)
+        ids (into [] (range 1 (+ (count cqs) 1)))
+        ]
+
+    (println (str "There are " (count cqs) " competency questions."))
+
+    (doseq [i (range 0 (count cqs))]
+      (let [name (str "cq" (get ids i))
+            cquestion (get cqs i)]
+        (create-cq name cquestion)
+        ))
+
+    ))
