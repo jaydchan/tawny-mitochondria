c371d8a60f8ee448d29df652192c78967ddebc5d
Jennifer Warrender <jaydchan@gmail.com>
2015-04-20 04:02:06 +0100

    basic reasoner tests for ontology

---
+++ test/ncl/mitochondria/disease_test.clj
@@ -0,0 +1,37 @@
+;; The contents of this file are subject to the LGPL License, Version 3.0.
+
+;; Copyright (C) 2014-2015, Newcastle University
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
+(ns ncl.mitochondria.disease_test
+  (:use [clojure.test])
+  (:require
+   [ncl.mitochondria.disease :as d]
+   [tawny.owl :as o]
+   [tawny.reasoner :as r]))
+
+(defn ontology-reasoner-fixture [tests]
+  (r/reasoner-factory :hermit)
+  (o/ontology-to-namespace d/disease)
+  (binding [r/*reasoner-progress-monitor*
+            (atom
+             r/reasoner-progress-monitor-silent)]
+    (tests)))
+
+(use-fixtures :once ontology-reasoner-fixture)
+
+(deftest Basic
+  (is (r/consistent?))
+  (is (r/coherent?)))
